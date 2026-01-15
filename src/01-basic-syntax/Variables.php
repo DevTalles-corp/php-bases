ba7bf534995c3c972c89eb15a2ef5b95f6abb47e
Teddy Paz <pazteddy@gmail.com>
2026-01-15 16:48:39 -0400

    Add examples of arrays, constants, and data types in Variables.php

--- src/01-basic-syntax/Variables.php
+++ src/01-basic-syntax/Variables.php
@@ -10,3 +10,17 @@ $isAvailable = false;
 echo "¿Disponible? " . ($isAvailable ? "Si" : "No") . PHP_EOL;
 $empty = null;
 echo "Valor nulo: " . var_export($empty, true) . PHP_EOL;
+$colors = array("rojo", "amarillo", "verde");
+$fruits = ["manzana", "platano", "pera"];
+echo "Frutas: " . implode(", ", $fruits) . PHP_EOL;
+$person =
+    [
+        'name' => 'Devi',
+        'age' => 23,
+        'email' => 'devi@example.com'
+    ];
+echo "Nombre: {$person['name']}" . PHP_EOL;
+const API_URL = "https://api.example.com";
+echo "URL: " . API_URL . PHP_EOL;
+echo "Tipo de dato de edad: " . gettype($age) . PHP_EOL;
+echo "Tipo de dato de Altura: " . gettype($height) . PHP_EOL;
