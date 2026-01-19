29b60d7b6b1d8d1597b7daeef69a811d74b6e732
Teddy Paz <pazteddy@gmail.com>
2026-01-19 16:22:11 -0400

    Add Operators.php with examples of PHP operators and basic purchasing logic

---
+++ src/01-basic-syntax/Operators.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+echo "🛒 Operadores en PHP\n\n";
+
+$price = 100;
+$qty = 1;
+$stock = 2;
+$coupon = "DEV10";
+
+$canBuy = ($qty <= $stock) && ($qty > 0); // false
+
+echo "Stock: $stock | Pedido: $qty\n";
+echo "¿Se puede comprar? " . ($canBuy ? "Sí ✅" : "No ❌") . "\n";
+if (!$canBuy) {
+    echo "Motivo: stock insuficiente o cantidad inválida \n";
+    exit;
+}
+$subototal = $price * $qty;
+$hasDiscount = ($coupon == "DEV10") || ($qty > 5);
+$discount = 0;
+if ($hasDiscount) {
+    $discount = $subototal * 0.10;
+}
+$total = 0.0;
+$total += $subototal;
+$total -= $discount;
+$stock -= $qty;
+$orderNumber = 99;
+echo "Orden # " . ++$orderNumber . " confirmada\n";
+echo "Stock Final: $stock\n";
