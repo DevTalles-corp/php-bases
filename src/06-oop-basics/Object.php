192181a256bea4e6c5c536d09c9f5303c039102a
Teddy Paz <pazteddy@gmail.com>
2026-02-06 17:52:41 -0400

    Refactor User class to include ID and password in constructor, and update user instantiation

--- src/06-oop-basics/Object.php
+++ src/06-oop-basics/Object.php
@@ -4,22 +4,41 @@ declare(strict_types=1);
 
 class User
 {
+    private int $id;
     public string $name;
     public string $email;
+    private string $password;
 
-    public function __construct(string $name, string $email)
+    public function __construct(int $id, string $name, string $email, string $password)
     {
+        $this->id = $id;
         $this->name = $name;
         $this->email = $email;
+        $this->password = $password;
     }
     public function showProfile()
     {
         echo "Nombre: {$this->name}, Email: {$this->email}\n";
     }
+    public function verifyPassword(string $password): bool
+    {
+        return $this->password === $password;
+    }
+    public function getId(): int
+    {
+        return $this->id;
+    }
 }
 
-$user = new User("Devi", "devi@mail.com");
+$user = new User(1, "Devi", "devi@mail.com", "123456");
+$user->name = "Teddy";
+
 echo "Objeto user fue creado correctamente\n";
 var_dump($user);
 echo "Su propiedad nombre tiene el valor: $user->name\n";
 $user->showProfile();
+echo $user->verifyPassword("1234568")
+    ? "Contraseña correcta\n"
+    : "Contraseña incorrecta\n";
+
+echo "ID: {$user->getId()}\n";
