6ed29919ffe540335177bfe593162c7364ba9abd
Teddy Paz <pazteddy@gmail.com>
2026-02-11 21:12:57 -0400

    Update composer.json autoload namespace and enhance Index.php with User and UserService integration

--- src/07-composer/src/Index.php
+++ src/07-composer/src/Index.php
@@ -1,6 +1,9 @@
 <?php
 require_once '../vendor/autoload.php';
 
+use App\Models\User;
+use App\Services\UserService;
+
 $quote = new \RandomQuotes\RandomQuotes();
 
 //print_r($quote->generate()); //[quoteAuthor] => Lao Tzu
@@ -9,3 +12,8 @@ $dayQuote = $quote->generate();
 echo "\"{$dayQuote['quoteText']}\"" . PHP_EOL;
 echo "Author: {$dayQuote['quoteAuthor']}";
 echo "\n";
+
+$user = new User();
+$user->name = "Devi";
+$service = new UserService();
+$service->register($user);
